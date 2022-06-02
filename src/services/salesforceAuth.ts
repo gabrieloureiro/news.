35e50f8c821521982d89643b5ff4f35006296763
Gabriel Loureiro <gabrieloureirof@gmail.com>
2022-06-02 19:22:52 -0300

    fix: try to fix cors error

--- src/services/salesforceAuth.ts
+++ src/services/salesforceAuth.ts
@@ -2,24 +2,22 @@ import axios from "axios";
 import qs from "qs";
 
 export async function salesforceAuth() {
-  const { data } = await axios.post(
-    process.env.NEXT_PUBLIC_SALESFORCE_OAUTH_URL,
-    qs.stringify({
+  return axios(process.env.NEXT_PUBLIC_SALESFORCE_OAUTH_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+    },
+    data: qs.stringify({
       grant_type: "password",
       client_id: process.env.NEXT_PUBLIC_SALESFORCE_CLIENT_ID,
       client_secret: process.env.NEXT_PUBLIC_SALESFORCE_CLIENT_SECRET,
       username: process.env.NEXT_PUBLIC_SALESFORCE_USERNAME,
       password: process.env.NEXT_PUBLIC_SALESFORCE_PASSWORD,
     }),
-    {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods":
-          "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
-      },
-    }
-  );
-  return data;
+  }).then((response) => {
+    return response.data;
+  });
 }
